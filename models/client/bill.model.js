2f3e26daf7ba181340cdc74b2098e3b58c01652e
Tys7603 <long.vlgt@gmail.com>
2023-12-29 18:10:56 +0700

    update api bill and table

--- models/client/bill.model.js
+++ models/client/bill.model.js
@@ -1,11 +1,37 @@
 const database = require('../../database/database')
 
-// tương tác với database , thực hiện câu lệnh thêm đơn hàng vào db
-const insertBill = async (id, timeOut, datePayment, tableID, userID) => {
+// tương tác với database , thực hiện câu lệnh thêm hóa đơn vào db
+const insertBillModel = async (id, timeOut, datePayment, tableID, userID) => {
     const query = "INSERT INTO HoaDon(id, gioRa, ngayThanhToan, banID, nguoiDungID) VALUES(?, ?, ?, ?, ?)"
     return await database.queryDatabase(query, [id, timeOut, datePayment, tableID, userID])
 }
 
+// tương tác với database , thực hiện câu lệnh lấy thông tin hóa đơn và bàn
+const readBillByTableIDModel = async (tableID) => {
+    const query = "SELECT HoaDon.id, HoaDon.gioVao, Ban.soThuTu, Ban.trangThaiOrder FROM HoaDon " +
+        "RIGHT JOIN Ban ON HoaDon.banID = Ban.id " +
+        "WHERE Ban.id = ?"
+    return await database.queryDatabase(query, [tableID])
+}
+
+// tương tác với database , thực hiện câu lệnh lấy thông tin hóa đơn chi tiết
+const readBillDetailModel = async (billID) => {
+    const query = "SELECT HoaDonChiTiet.id, HoaDonChiTiet.soLuong, SanPham.anhSanPham, SanPham.tenSanPham, SanPham.giaSanPham FROM HoaDonChiTiet " +
+        "INNER JOIN SanPham ON HoaDonChiTiet.sanPhamID = SanPham.id " +
+        "WHERE HoaDonChiTiet.hoaDonID = ?"
+    return await database.queryDatabase(query, [billID])
+}
+
+
+// tương tác với database , thực hiện câu lệnh thêm hóa đơn chi tiết vào db
+const insertBillDetailModel = async (quantity, intoMoney, productID, billID) => {
+    const query = "INSERT INTO HoaDonChiTiet(soLuong, thanhTien, sanPhamID, hoaDonID) VALUES(?, ?, ?, ?)"
+    return await database.queryDatabase(query, [quantity, intoMoney, productID, billID])
+}
+
 module.exports = {
-    insertBill
+    insertBillModel,
+    readBillByTableIDModel,
+    readBillDetailModel,
+    insertBillDetailModel
 }
--- routers/client/bill.router.js
+++ routers/client/bill.router.js
@@ -3,5 +3,10 @@ const controller = require('../../controllers/client/bill.controller')
 
 router.post("/insert/bill", controller.insertBill)
 
+router.post("/insert/bill/detail", controller.insertBillDetail)
+
+router.get("/table/bill/:tableID", controller.readBillByTable)
+
+router.get("/detail/bills/:billID", controller.readBillDetail)
 
 module.exports = router
